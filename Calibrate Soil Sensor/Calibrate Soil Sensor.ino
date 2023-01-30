c609325c391081574f20adbc2199ec09ea21456f
bvanwie <117214015+bvanwie@users.noreply.github.com>
2023-01-30 15:59:45 -0500

    Implements min (water) and max (air) values for a sensor.
    Add two variables for rolling min/max of the sensor reading. The max value (airMax) indicates no moisture and the min value (waterMin) indicates the sensor is submerged in water. Start these variables at a center point and they will make their way to the extremes as the sensor is submerged or dried. Keep these values to use as the calibration baseline for other projects using the sensors.

--- Calibrate Soil Sensor/Calibrate Soil Sensor.ino
+++ Calibrate Soil Sensor/Calibrate Soil Sensor.ino
@@ -2,6 +2,8 @@ const int AirValue = 568;   //you need to replace this value with Value_1
 const int WaterValue = 305;  //you need to replace this value with Value_2
 int soilMoistureValue = 0;
 int soilmoisturepercent=0;
+int airMax = 450;   // Choose a number that should be around 1/2way between wet and dry as a starting point.
+int waterMin = 450; // Choose a number that should be around 1/2way between wet and dry as a starting point.
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); // open serial port, set the baud rate to 9600 bps
@@ -12,7 +14,7 @@ void reportMoisturePercent() {
   // Maps the current moisture value to a 1-100 scale based on pre-determined AirValue and WaterValue constraints.
   // Prints the percentage without a /n so that more data can be added to the line.
   // Is the 'if' statement needed when the map statement is already in place?
-  soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
+  soilmoisturepercent = map(soilMoistureValue, airMax, waterMin, 0, 100);
   if(soilmoisturepercent >= 100)
   {
     Serial.print("\t100 %");
@@ -32,9 +34,21 @@ void reportMoisturePercent() {
 void loop() {
   // put your main code here, to run repeatedly:
   soilMoistureValue = analogRead(A0);  //put Sensor insert into soil
+  if (soilMoistureValue < waterMin) 
+  {
+    waterMin = soilMoistureValue;
+  }
+  if (soilMoistureValue > airMax)
+  {
+    airMax = soilMoistureValue;
+  }
   Serial.print("Sensor reading: ");
   Serial.print(soilMoistureValue);
   reportMoisturePercent(); // Call the procedure to print the % moisture.
+  Serial.print("\tWet reading: ");
+  Serial.print(waterMin);
+  Serial.print("\tDry reading: ");
+  Serial.print(airMax);
   Serial.println();
   delay(250);
 }
